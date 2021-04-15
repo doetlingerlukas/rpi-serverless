71b7ece809673b8d510c2a87bc022f16db182bf4
Lukas Dötlinger <lukas.doetlinger@student.uibk.ac.at>
2021-04-15 12:41:22 +0200

    Update 'addition'.

--- functions/addition/handler.rb
+++ functions/addition/handler.rb
@@ -1,5 +1,12 @@
+require 'json'
+
 class Handler
   def run(req)
-    return req.delete(' ').split(',').map(&:to_i).reduce(0, :+)
+    req_json = JSON.parse(req)
+
+    sleep(req_json['waitTimeIn'].to_i / 1000)
+
+    result = { 'sum' => req_json['firstSummand'].to_i + req_json['secondSummand'].to_i }
+    return result.to_json
   end
 end
