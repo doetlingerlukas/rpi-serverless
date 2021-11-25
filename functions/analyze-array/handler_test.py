fc9403b7007eaf16ece74c07ca09f832659821fc
Lukas Dötlinger <lukas.doetlinger@student.uibk.ac.at>
2021-11-25 23:52:06 +0100

    Update `analyze-array` function.

--- functions/analyze-array/handler_test.py
+++ functions/analyze-array/handler_test.py
@@ -2,10 +2,14 @@ from .handler import handle
 import json
 
 def test_handle():
-  input_str = json.dumps({
-    "body": list(range(1, 100))
-  })
-  result = handle(input_str, None)
+  input_str = json.dumps(list(range(1, 100)))
+  result, status = handle(input_str)
 
-  assert result['statusCode'] == 200
-  assert result['body']['median'] == 50
+  assert status == 200
+  assert result['median'] == 50
+
+  input_str = json.dumps(list([0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]))
+  result, status = handle(input_str)
+
+  assert status == 200
+  assert result['median'] == 0.55
